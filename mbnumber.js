c929c881ef1883740d0292280b6cdf3de1d00a5f
evildead <danilo.carrabino@gmail.com>
2018-08-30 18:50:18 +0200

    Added methods to check values passed in input and transform binary numbers to decimal numbers and viceversa

--- mbnumber.js
+++ mbnumber.js
@@ -3,13 +3,48 @@ const MBNumber = class MBNumber {
         if(!arguments.length) {
             throw new Error('value and base required');
         }
-        else if(arguments.length < 2) {
-            throw new Error('base required too');
-        }
         else {
             this.validBaseNumbers = [2, 10];
-            this.checkValidBase(base);
+            if(arguments.length < 2) {
+                // default base is 10
+                this.checkValidBase(10);
+            }
+            else {
+                this.checkValidBase(base);
+            }
+
+            this.checkValidValue(val);
         }
+    }
+
+    /**
+     * function fromBinaryValToBase10
+     * @param {*} val must be a string
+     */
+    static fromBinaryValToBase10(val) {
+        if(!(typeof(val) === "string" || val instanceof String)) {
+            throw new Error('value is not a valid integer number in base 2');
+        }
+
+        if(!val.length) {
+            throw new Error('value is not a valid integer number in base 2');
+        }
+
+        let base10Val = 0;
+        let power = 0;
+
+        for(let i = val.length - 1; i >= 0; i--) {
+            let currChar = val.charAt(i);
+            if(currChar == '1') {
+                base10Val += Math.pow(2, power);
+            }
+            else if(currChar != '0') {
+                throw new Error('value is not a valid integer number in base 2');
+            }
+            power++;
+        }
+
+        return base10Val;
     }
 
     checkValidBase(val) {
@@ -24,8 +59,52 @@ const MBNumber = class MBNumber {
             this.base = intVal;
         }
         else {
-            throw new Error('base in not a valid integer number');
+            throw new Error('base is not a valid integer number');
         }
+    }
+
+    checkValidValue(val) {
+        switch(this.base) {
+        case 2:
+        {
+            if( (Number.isInteger(val)) ||
+                ((typeof(val) === "string" || val instanceof String) && Number.isInteger(parseInt(val))) ) {
+                
+                let intVal = parseInt(val);
+                this.base2Val = intVal.toString();
+                this.base10Val = MBNumber.fromBinaryValToBase10(this.base2Val);
+            }
+            else {
+                throw new Error('value is not a valid integer number in base 2');
+            }
+        }
+            break;
+        
+        case 10:
+        {
+            if( (Number.isInteger(val)) ||
+                ((typeof(val) === "string" || val instanceof String) && Number.isInteger(parseInt(val))) ) {
+                
+                this.base10Val = parseInt(val);
+                this.base2Val = this.base10Val.toString(2);
+            }
+            else {
+                throw new Error('value is not a valid integer number in base 10');
+            }
+        }
+            break;
+        
+        default:
+            break;
+        }
+    }
+
+    toBase2() {
+        return this.base2Val;
+    }
+
+    toBase10() {
+        return this.base10Val;
     }
 };
 
--- test/testMBNumber.js
+++ test/testMBNumber.js
@@ -18,32 +18,58 @@ describe('MBNumber', () => {
             expect(() => new MBNumber()).to.throw(Error, /^value and base required$/);
         });
 
-        it('The constructor invoked with only one element should return an exception: base required too', () => {
-            expect(() => new MBNumber(54)).to.throw(Error, /^base required too$/);
-        });
-
-        it('The constructor invoked with a non integer base should return an exception: base in not a valid integer number', () => {
-            expect(() => new MBNumber(23, 'inv')).to.throw(Error, /^base in not a valid integer number$/);
+        it('The constructor invoked with a non integer base should return an exception: base is not a valid integer number', () => {
+            expect(() => new MBNumber(23, 'inv')).to.throw(Error, /^base is not a valid integer number$/);
         });
 
         it('The constructor invoked with an integer base but different from 2 and 10 should return an exception: invalid base', () => {
-            expect(() => new MBNumber(23, 7)).to.throw(Error, /^invalid base$/);
+            expect(() => new MBNumber(5213, 7)).to.throw(Error, /^invalid base$/);
+        });
+
+        it('The constructor invoked with a single value "null" should return an exception: value is not a valid integer number in base 10', () => {
+            expect(() => new MBNumber(null)).to.throw(Error, /^value is not a valid integer number in base 10$/);
+        });
+
+        it('The constructor invoked with an incorrect binary number should return an exception: value is not a valid integer number in base 2', () => {
+            expect(() => new MBNumber('6a5', 2)).to.throw(Error, /^value is not a valid integer number in base 2$/);
         });
     });
 
-    /*
     describe('Check values', () => {
-        it('The arabic number 1 should be equal to the Roman number "I"', () => {
-            let romannum = RomanNumber(1);
-            romannum.toString().should.equal('I');
-            romannum.toInt().should.equal(1);
+        it('The binary number "0" should be equal to the decimal number 0', () => {
+            let mbNumber = new MBNumber('0', 2);
+            mbNumber.toBase2().should.equal('0');
+            mbNumber.toBase10().should.equal(0);
         });
 
-        it('The Roman number "MMMCMXCIX" should be equal to the arabic number 3999', () => {
-            let romannum = RomanNumber('MMMCMXCIX');
-            romannum.toString().should.equal('MMMCMXCIX');
-            romannum.toInt().should.equal(3999);
+        it('The binary number "1" should be equal to the decimal number 1', () => {
+            let mbNumber = new MBNumber('1', 2);
+            mbNumber.toBase2().should.equal('1');
+            mbNumber.toBase10().should.equal(1);
+        });
+        
+        it('The binary number "11011" should be equal to the decimal number 27', () => {
+            let mbNumber = new MBNumber('11011', 2);
+            mbNumber.toBase2().should.equal('11011');
+            mbNumber.toBase10().should.equal(27);
+        });
+
+        it('The binary number "10001101" should be equal to the decimal number 141', () => {
+            let mbNumber = new MBNumber(10001101, 2);
+            mbNumber.toBase2().should.equal('10001101');
+            mbNumber.toBase10().should.equal(141);
+        });
+
+        it('The decimal number 7 should be equal to the binary number "111"', () => {
+            let mbNumber = new MBNumber(7);
+            mbNumber.toBase2().should.equal('111');
+            mbNumber.toBase10().should.equal(7);
+        });
+
+        it('The decimal number 2018 should be equal to the binary number "11111100010"', () => {
+            let mbNumber = new MBNumber('2018', 10);
+            mbNumber.toBase2().should.equal('11111100010');
+            mbNumber.toBase10().should.equal(2018);
         });
     });
-    */
 });
